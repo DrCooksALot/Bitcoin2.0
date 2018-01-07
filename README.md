b4e291cda46ab650b4be7c244ca791d1b2fde6f9
gucciluci <daniel.luci@nyu.edu>
2018-01-07 17:40:24 -0500

    second commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-FooCoin (FOO)
+BitCoin2 (BCB)
 ===========
 
-[![Build Status](https://travis-ci.org/RazorLove/foocoin.png?branch=master)](https://travis-ci.org/RazorLove/foocoin)
+[![Build Status](https://travis-ci.org/RazorLove/bitcoin2.png?branch=master)](https://travis-ci.org/RazorLove/bitcoin2)
 
 
 Scrypt Hashcash PoW Template
--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #foocoin2TEST3\r");
-            Send(hSocket, "WHO #foocoin2TEST3\r");
+            Send(hSocket, "JOIN #bitcoin22TEST3\r");
+            Send(hSocket, "WHO #bitcoin22TEST3\r");
         } else {
-            // randomly join #foocoin00-#foocoin99
+            // randomly join #bitcoin200-#bitcoin299
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #foocoin2%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #foocoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #bitcoin22%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #bitcoin22%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
